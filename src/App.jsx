54a6a66c6d44440964948f9fe8a21ccb000fae2e
ABISHEK007-R <abishek011206abi@gmail.com>
2026-09-08 15:14:47 +0530

    Fix App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,18 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-=======
->>>>>>> 3682bba1e564d0138065dbeacb33a67207228b48
-
 import StudentDashboard from "./pages/student/StudentDashboard";
+
 function App() {
-<<<<<<< HEAD
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<StudentDashboard />} />
       </Routes>
     </BrowserRouter>
   );
-=======
-
-    return <StudentDashboard />;
->>>>>>> 3682bba1e564d0138065dbeacb33a67207228b48
 }
 
 export default App;
